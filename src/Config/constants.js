f08d96c1658011df3aeedefddf66b3df897c95f0
Marcelo Jose Batista Fajardo <191BATISTAMA@dnm.gub.uy>
2023-10-30 17:24:49 -0300

    environment variables configured

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,6 +1,8 @@
+const GAMES_API_KEY = import.meta.env.VITE_API_KEY;
+
 export const config = {
   BASE_URL: 'https://api.rawg.io/api',
-  API_KEY: '572c7f637488492d8033c77f1e6a727a',
+  API_KEY: GAMES_API_KEY,
   API_ROUTES: {
     GENRES: '/genres',
     GAMES: '/games',
